cf0a8203b70deea0a9157a2f9ad7e1c9afb10b94
Raj Mishra <rajm@deloitte.com>
2020-01-08 22:23:40 +0530

    first commit

---
+++ core/src/main/java/org/sarb/reservebank/models/CustomHeadingModel.java
@@ -0,0 +1,46 @@
+package org.sarb.reservebank.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = SlingHttpServletRequest.class,
+    resourceType = CustomHeadingModel.RESOURCE_TYPE,
+    adapters = {CustomHeadingModel.class, ComponentExporter.class},
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class CustomHeadingModel implements ComponentExporter {
+
+  protected static final String RESOURCE_TYPE = "reservebank/components/content/custom-heading";
+
+  @ValueMapValue(name = "heading")
+  private String heading;
+
+  @ValueMapValue(name = "headingType")
+  private String headingType;
+
+  @ValueMapValue(name = "headingColor")
+  private String headingColor;
+
+  public String getHeading() {
+    return heading;
+  }
+
+  public String getHeadingType() {
+    return headingType;
+  }
+
+  public String getHeadingColor() {
+    return headingColor;
+  }
+
+ 
+  @Override
+  public String getExportedType() {
+    return RESOURCE_TYPE;
+  }
+}
